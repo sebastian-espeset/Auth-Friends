b0d2e69fae10ab8acd2b177f88d4d783a3b41241
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-01-26 15:09:23 -0600

    server passes test

--- auth-friends-client/src/App.js
+++ auth-friends-client/src/App.js
@@ -1,6 +1,19 @@
+import { axiosWithAuth } from './utils/axiosWithAuth';
+
+
+//want to test the server, see if I can store the token correctly....
+
+
 
 
 function App() {
+ axiosWithAuth().get("http://localhost:5000/api/friends")
+  .then((res)=>{
+    console.log(res)
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
   return (
     <div >
       <h1>Hello world</h1>
---
+++ auth-friends-client/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export default function Login() {
+    const [signIn, setSignIn]=useState({username:'',password:''})
+
+
+    return (
+        <div>
+            <input type='text' name='username' value={state.username}>username</input>
+            <input type='text' name='password' value={state.password}>password</input>
+        </div>
+    )
+}
